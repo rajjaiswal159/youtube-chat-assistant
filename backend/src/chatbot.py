4383725658cecb10828034e66382691467e5c6cd
rajjaiswal159 <rajjeswal31@gmail.com>
2026-07-01 19:50:09 +0530

    Update chatbot.py

--- backend/src/chatbot.py
+++ backend/src/chatbot.py
@@ -1,7 +1,6 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
 from langchain_core.runnables import RunnableLambda
 
 
