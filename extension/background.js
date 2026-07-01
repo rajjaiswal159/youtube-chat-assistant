0db736598f9906e20f97b4c55ced064359e99acd
rajjaiswal159 <rajjeswal31@gmail.com>
2026-07-01 19:23:06 +0530

    Update background.js

--- extension/background.js
+++ extension/background.js
@@ -1,4 +1,7 @@
+// Open the side panel when the extension is installed
 chrome.runtime.onInstalled.addListener(() => {
+    
+    // Enable opening the side panel by clicking the extension icon
     chrome.sidePanel.setPanelBehavior({
         openPanelOnActionClick: true
     });
